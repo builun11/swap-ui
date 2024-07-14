44282049b41f7fbe1fe291d45a61fb59bfb6745f
builun11 <builun11@gmail.com>
2024-07-14 14:20:39 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,97 @@
+import { useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  BloctoWalletAdapter,
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  SolletWalletAdapter,
+  SolongWalletAdapter,
+  Coin98WalletAdapter,
+  CloverWalletAdapter,
+  TorusWalletAdapter,
+  MathWalletAdapter,
+  SlopeWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { ToastContainer, toast } from "react-toastify";
+import Footer from "./components/navigation-frame/Footer";
+import TopBar from "./components/navigation-frame/TopBar";
+import { RPC_URL, RPC_WSS } from "./settings/rpc";
+import { JupiterApiProvider } from "./contexts";
+import { Buffer } from "buffer";
+import JupiterForm from "./components/Jupiter";
+import { useLocalStorageState } from "ahooks";
+import { tokenAuthFetchMiddleware } from "@strata-foundation/web3-token-auth";
+import { getToken } from "@bonfida/hooks";
+import { Warning } from "./components/Warning";
+
+import "react-toastify/dist/ReactToastify.css";
+
+// Override @solana/wallet-adapter-react-ui/styles.css
+import "./wallet.css";
+
+window.Buffer = Buffer;
+
+const App = () => {
+  const [customRpc, setCustomRpc] = useLocalStorageState<string>("customRpc");
+  const [visible, setVisible] = useLocalStorageState<boolean>("warning", {
+    defaultValue: true,
+  });
+
+  const network = WalletAdapterNetwork.Mainnet;
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SlopeWalletAdapter(),
+      new SolletWalletAdapter({ network }),
+      new SolflareWalletAdapter(),
+      new TorusWalletAdapter(),
+      new SolongWalletAdapter(),
+      new MathWalletAdapter(),
+      new Coin98WalletAdapter(),
+      new CloverWalletAdapter(),
+      new BloctoWalletAdapter({ network }),
+    ],
+    [network]
+  );
+
+  const endpoint = useMemo(() => customRpc || (RPC_URL as string), [customRpc]);
+
+  const jwt = endpoint?.includes("quiknode");
+
+  return (
+    <ConnectionProvider
+      endpoint={endpoint as string}
+      config={{
+        commitment: "processed",
+        confirmTransactionInitialTimeout: 15 * 1_000,
+        wsEndpoint: RPC_WSS,
+        fetchMiddleware: jwt
+          ? tokenAuthFetchMiddleware({
+              getToken,
+              tokenExpiry: 2.5 * 60 * 1_000,
+            })
+          : undefined,
+      }}
+    >
+      <WalletProvider wallets={wallets} autoConnect>
+        <JupiterApiProvider>
+          <div className="bg-neutral">
+            <TopBar setCustomRpc={setCustomRpc} />
+            <div className="flex flex-col items-center justify-center min-h-screen bg-neutral">
+              <JupiterForm />
+            </div>
+            <Footer />
+          </div>
+          <Warning visible={visible} setVisible={setVisible} />
+        </JupiterApiProvider>
+      </WalletProvider>
+      <ToastContainer position={toast.POSITION.BOTTOM_LEFT} />
+    </ConnectionProvider>
+  );
+};
+
+export default App;
---
+++ src/components/Jupiter/Balance.tsx
@@ -0,0 +1,69 @@
+import { TokenInfo } from "@solana/spl-token-registry";
+import { TokenAccounts } from "@bonfida/hooks";
+import round from "lodash/round";
+import { PublicKey } from "@solana/web3.js";
+import { useWallet, useConnection } from "@solana/wallet-adapter-react";
+import { NATIVE_MINT } from "@solana/spl-token";
+
+export const Balance = ({
+  token,
+  tokenAccounts,
+  setInput,
+  solBalance,
+}: {
+  token: TokenInfo | null | undefined;
+  tokenAccounts: TokenAccounts | undefined;
+  setInput?: (value: React.SetStateAction<string>) => void;
+  solBalance:
+    | {
+        amount: number;
+        uiAmount: number;
+      }
+    | undefined;
+}) => {
+  const { connection } = useConnection();
+  const { publicKey } = useWallet();
+
+  const wSol = token?.address === NATIVE_MINT.toBase58();
+
+  const tokenAccount = token
+    ? tokenAccounts?.getByMint(new PublicKey(token?.address))
+    : null;
+
+  if (!token) {
+    return null;
+  }
+
+  const balance =
+    tokenAccount && tokenAccount.decimals
+      ? Number(tokenAccount.account.amount) /
+        Math.pow(10, tokenAccount.decimals)
+      : 0;
+
+  return (
+    <div className="flex flex-row items-center mr-1">
+      <span className="mr-1 text-sm font-bold text-white">Balance: </span>
+      <span className="mr-1 text-sm font-bold text-white opacity-40">
+        {" "}
+        {round(wSol ? solBalance?.uiAmount || 0 : balance, 2)}
+      </span>
+
+      {setInput && !!balance && (
+        <>
+          <div
+            onClick={() => setInput((balance / 2).toString())}
+            className="border-[2px] border-solid rounded-[20px] border-sky-500 w-[50px] text-center text-xs cursor-pointer uppercase font-bold mr-1"
+          >
+            Half
+          </div>
+          <div
+            onClick={() => setInput(balance.toString())}
+            className="border-[2px] border-solid rounded-[20px] border-sky-500 w-[50px] text-center text-xs cursor-pointer uppercase font-bold"
+          >
+            Max
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
